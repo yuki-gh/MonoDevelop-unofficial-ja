c6192f4b6f88610a6f6d4bc01da8197c8be23340
Takayuki Ito <yuki@euc.jp>
2016-07-29 15:13:26 +0900

    beta removed

--- install_win.bat
+++ install_win.bat
@@ -4,10 +4,7 @@ cd /d "%~dp0"
 set dir=bin\locale\ja\LC_MESSAGES
 mkdir "%ProgramFiles%\Unity\MonoDevelop\%dir%"
 copy /-y monodevelop.mo "%ProgramFiles%\Unity\MonoDevelop\%dir%"
-mkdir "%ProgramFiles%\UnityBeta\MonoDevelop\%dir%"
-copy /-y monodevelop.mo "%ProgramFiles%\UnityBeta\MonoDevelop\%dir%"
 copy /-y monodevelop.mo "%ProgramFiles(x86)%\Xamarin Studio\%dir%"
 
-copy /-y TipsOfTheDay.xml "%ProgramFiles%\Unity\MonoDevelop\data\options\TipsOfTheDay.xml"
-copy /-y TipsOfTheDay.xml "%ProgramFiles%\UnityBeta\MonoDevelop\data\options\TipsOfTheDay.xml"
-copy /-y TipsOfTheDay.xml "%ProgramFiles(x86)%\Xamarin Studio\data\options\TipsOfTheDay.xml"
+copy /-y TipsOfTheDay.xml "%ProgramFiles%\Unity\MonoDevelop\data\options"
+copy /-y TipsOfTheDay.xml "%ProgramFiles(x86)%\Xamarin Studio\data\options"
