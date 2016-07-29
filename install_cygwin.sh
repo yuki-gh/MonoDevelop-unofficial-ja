45c7a0ece74e786fa4b2f34cdb82ed46df5fb76b
Takayuki Ito <yuki@euc.jp>
2016-07-29 15:14:17 +0900

    first

---
+++ install_cygwin.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+dir=bin/locale/ja/LC_MESSAGES
+PROGRAMFILES_X86="$PROGRAMFILES (x86)"
+
+mkdir -p "$PROGRAMFILES/Unity/MonoDevelop/$dir"
+cp -i monodevelop.mo "$PROGRAMFILES/Unity/MonoDevelop/$dir"
+cp -i monodevelop.mo "$PROGRAMFILES_X86/Xamarin Studio/$dir"
+
+cp -i TipsOfTheDay.xml "$PROGRAMFILES/Unity/MonoDevelop/data/options"
+cp -i TipsOfTheDay.xml "$PROGRAMFILES_X86/Xamarin Studio/data/options"
