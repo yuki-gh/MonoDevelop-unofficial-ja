7b10580916618fcc29761f43912bcb2fca533373
Takayuki Ito <yuki@euc.jp>
2016-08-10 23:06:23 +0900

    x86/x64

--- install_cygwin.sh
+++ install_cygwin.sh
@@ -5,7 +5,11 @@ PROGRAMFILES_X86="$PROGRAMFILES (x86)"
 
 mkdir -p "$PROGRAMFILES/Unity/MonoDevelop/$dir"
 cp -i monodevelop.mo "$PROGRAMFILES/Unity/MonoDevelop/$dir"
+cp -i monodevelop.mo "$PROGRAMFILES_X86/Unity/MonoDevelop/$dir"
+cp -i monodevelop.mo "$PROGRAMFILES/Xamarin Studio/$dir"
 cp -i monodevelop.mo "$PROGRAMFILES_X86/Xamarin Studio/$dir"
 
 cp -i TipsOfTheDay.xml "$PROGRAMFILES/Unity/MonoDevelop/data/options"
+cp -i TipsOfTheDay.xml "$PROGRAMFILES_X86/Unity/MonoDevelop/data/options"
+cp -i TipsOfTheDay.xml "$PROGRAMFILES/Xamarin Studio/data/options"
 cp -i TipsOfTheDay.xml "$PROGRAMFILES_X86/Xamarin Studio/data/options"
--- install_win.bat
+++ install_win.bat
@@ -4,7 +4,11 @@ cd /d "%~dp0"
 set dir=bin\locale\ja\LC_MESSAGES
 mkdir "%ProgramFiles%\Unity\MonoDevelop\%dir%"
 copy /-y monodevelop.mo "%ProgramFiles%\Unity\MonoDevelop\%dir%"
+copy /-y monodevelop.mo "%ProgramFiles(x86)%\Unity\MonoDevelop\%dir%"
+copy /-y monodevelop.mo "%ProgramFiles%\Xamarin Studio\%dir%"
 copy /-y monodevelop.mo "%ProgramFiles(x86)%\Xamarin Studio\%dir%"
 
 copy /-y TipsOfTheDay.xml "%ProgramFiles%\Unity\MonoDevelop\data\options"
+copy /-y TipsOfTheDay.xml "%ProgramFiles(x86)%\Unity\MonoDevelop\data\options"
+copy /-y TipsOfTheDay.xml "%ProgramFiles%\Xamarin Studio\data\options"
 copy /-y TipsOfTheDay.xml "%ProgramFiles(x86)%\Xamarin Studio\data\options"
